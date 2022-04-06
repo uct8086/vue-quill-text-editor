51fd19f819fae46e94e2e8b998af2d5fbcd31258
uct8086 <jsontang@jsontangdeMac-mini.local>
2022-04-06 15:22:56 +0800

    feat: simple packaging

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # vue-quill-editor
 Quill rich text editor component encapsulated with Vue3
+
+
+# install
+
+    npm install
+
+# import into project:
+
+```javascript
+import VueQuillEditor from 'vue-quill-editor';
+import 'vue-quill-editor/dist/vue-quill-editor.esm.css';
+
+...
+
+app.use(VueQuillEditor);
+
+app.mount("#app-wrapper");
+
+```
+
--- src/quillComponent/index.vue
+++ src/quillComponent/index.vue
@@ -1,43 +1,48 @@
 <template>
-  <div id="quillId" class="quill-container"></div>
+  <div :id="quillId" class="quill-container"></div>
 </template>
 
 <script>
-import { onMounted, defineComponent } from "vue";
+import { onMounted, defineComponent, ref } from "vue";
 import Quill from "quill";
-// import { QuillProps } from "./props";
+import "quill/dist/quill.snow.css";
+import { QuillProps } from "./props";
 
 export default defineComponent({
   name: "VueQuillEditor",
-  // props: QuillProps,
-  emits: ["scroll"],
-  setup(props, { emit }) {
+  props: QuillProps,
+  // emits: ["blur", "focus", "disable", "enable", "hasFocus", "update"],
+  setup(props) {
     const date = +new Date();
     const quillId = `quill_${date}`;
+    const toolbarId = `toolbar_${date}`;
+    const quillInstance = ref();
 
     onMounted(() => {
-        const container = document.getElementById(quillId);
-        const Instance = new Quill(container, {
-            debug: 'info',
-            modules: {
-                toolbar: '#toolbar'
-            },
-            placeholder: 'Compose an epic...',
-            readOnly: true,
-            theme: 'snow'
-        });
-        console.log(Instance);
-    })
+      const toolbarOptions = props.toolbarOptions;
+      quillInstance.value = new Quill(`#${quillId}`, {
+        debug: "error",
+        modules: {
+          toolbar: toolbarOptions,
+        },
+        placeholder: props.placeholder,
+        theme: "snow",
+      });
+      console.log(quillInstance.value);
+    });
 
     return {
       quillId,
+      toolbarId,
+      quillInstance,
     };
   },
 });
 </script>
 
 <style lang="less" scoped>
-.quill-container{
-    margin: 0;
+.quill-container {
+  margin: 0;
+  height: 500px;
 }
 </style>
