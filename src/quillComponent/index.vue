d53e5a71f6a7b774d43403a46cec875dcbb8b9b2
uct8086 <belieftang@163.com>
2022-05-06 18:23:19 +0800

    feat: add image upload

--- src/quillComponent/index.vue
+++ src/quillComponent/index.vue
@@ -4,9 +4,11 @@
 
 <script>
 import { onMounted, defineComponent, ref } from "vue";
+import ImageUploader from 'quill-image-uploader';
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import { QuillProps } from "./props";
+Quill.register("modules/imageUploader", ImageUploader);
 
 export default defineComponent({
   name: "VueQuillTextEditor",
@@ -22,6 +24,17 @@ export default defineComponent({
         debug: "error",
         modules: {
           toolbar: toolbarOptions,
+          imageUploader: {
+            upload: (file) => {
+              return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  resolve(
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png"
+                  );
+                }, 3500);
+              });
+            },
+          },
         },
         placeholder: props.placeholder,
         theme: "snow",
